982bfaf0560a7c731db60d6c5b70248e9040b6e7
Mark <mark@gelfius.com>
2026-01-31 09:00:14 -0500

    cloudflare config

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,12 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from "@sveltejs/adapter-cloudflare";
 
-const dev = process.argv.includes('dev');
-
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-		}),
-		appDir: 'app',
-		paths: {
-            base: dev ? '' : '/fives',
-        }
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
